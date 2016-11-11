9bd1cb5c8ab9b056f9d6224d86bab459687a89a7
Anh Hoang <Anh.Hoang@bulpros.com>
2016-11-11 17:37:59 +0200

    step 3 - updates

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -5,6 +5,8 @@ import { HEROES } from './mock-heroes';
 @Injectable()
 export class HeroService {
     getHeroes(): Promise<Hero[]> {
-        return Promise.resolve(HEROES);
+        return new Promise<Hero[]>(resolve =>
+            setTimeout(resolve, 2000))
+            .then(() => this.getHeroes());
     }
 }
